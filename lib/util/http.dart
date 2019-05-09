a5e62db937db018d2397c3817bf61efa9af29778
weicheng <791279106@qq.com>
2019-05-09 14:32:30 +0800

    修复dio库超时时间无效的问题

--- lib/util/http.dart
+++ lib/util/http.dart
@@ -17,17 +17,19 @@ class Http{
   static const BASE_URL = "http://10.0.4.145:8080/";
 
   static Dio dio;
+  static final Options dioOptions = Options(connectTimeout: TIMEOUT_MILS,receiveTimeout: TIMEOUT_MILS);
 
   static Future<String> post(BuildContext context,String url,FormData data) async {
     try{
       print("HTTP POST：${BASE_URL}${url} ${data}" );
-      Response response = await getDio().post(url,data:data);
+      Response response = await getDio().post(url,data:data,options: dioOptions);
       print("RESPONSE：${BASE_URL}${url} ${response}");
       BaseResp baseResp = new BaseResp(response.data);
       if(baseResp.code != SUCCESS) Fluttertoast.showToast(msg: baseResp.msg);
       //if(baseResp.code == TOKEN_ERROR) _logout(context); //TODO：自动退出登录判断
       return response.toString();
-    }catch(e){
+    }on DioError catch(e){
+      print("HTTP ERROR：$e");
       Fluttertoast.showToast(msg: "网络错误");
       return BaseResp.fromParams(code:FAILED,msg:"网络错误").toString();
     }
@@ -37,8 +39,6 @@ class Http{
     if(dio != null) return dio;
     dio = new Dio();
     dio.options.baseUrl = BASE_URL;
-    dio.options.connectTimeout = TIMEOUT_MILS;
-    dio.options.receiveTimeout = TIMEOUT_MILS;
     return dio;
   }
 
